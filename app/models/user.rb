b734afadad83fa55ded5583c9f442abb1fb69da3
prudnikovv <prudnikovv@ukr.net>
2020-01-07 14:39:22 +0000

    improve model users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    :recoverable, :rememberable, :validatable
+
+  has_many :events
+
+  validates :name, presence: true, length: {maximum: 35}
+
+  validates :email, length: {maximum: 255}
+  validates :email, uniqueness: true
+  validates :email, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
 
   before_validation :set_name, on: :create
 
