ca7d49b8e9ba2f8c690031b86d57c5a6e74fd4fa
prudnikovv <prudnikovv@ukr.net>
2020-01-08 06:35:27 +0000

    improve application

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,7 @@
 class EventsController < ApplicationController
-  # Задаем объект @event для тех действий, где он нужен
-  before_action :set_event, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:show, :index]
+  before_action :set_event, only: [:show]
   before_action :set_current_user_event, only: [:edit, :update, :destroy]
-
 
   def index
     @events = Event.all
@@ -23,7 +21,7 @@ class EventsController < ApplicationController
     @event = current_user.events.build(event_params)
 
     if @event.save
-      redirect_to @event, notice: 'Event was successfully created.'
+      redirect_to @event, notice: I18n.t('controllers.events.created')
     else
       render :new
     end
@@ -31,7 +29,7 @@ class EventsController < ApplicationController
 
   def update
     if @event.update(event_params)
-      redirect_to @event, notice: 'Event was successfully updated.'
+      redirect_to @event, notice: I18n.t('controllers.events.updated')
     else
       render :edit
     end
@@ -39,7 +37,7 @@ class EventsController < ApplicationController
 
   def destroy
     @event.destroy
-    redirect_to events_url, notice: 'Event was successfully destroyed.'
+    redirect_to events_url, notice: I18n.t('controllers.events.destroyed')
   end
 
   private
@@ -52,7 +50,6 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
-  # редактируем параметры события
   def event_params
     params.require(:event).permit(:title, :address, :datetime, :description)
   end
