162f83d64448b54b654a4a4383e36df941a7954c
prudnikovv <prudnikovv@ukr.net>
2020-01-11 06:42:25 +0000

    Improve task 55-2

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,12 +2,11 @@ class Subscription < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :event
 
-  validates :event, presence: true
   validates :user_name, presence: true, unless: -> { user.present? }
-  validates :user_email, presence: true, format: /\A[\w\-.]+@[\w\-]+\.[\w\-.]+\z/, unless: -> { user.present? }
-
+  validates :user_email, presence: true, uniqueness: { scope: :event_id } ,
+              format: /\A[\w\-.]+@[\w\-]+\.[\w\-.]+\z/, unless: -> { user.present? }
   validates :user, uniqueness: { scope: :event_id } , if: -> { user.present? }
-  validates :user_email, uniqueness: { scope: :event_id } , unless: -> { user.present? }
+
   validate :unique_email, unless: -> { user.present? }
 
   def user_name
@@ -26,8 +25,8 @@ class Subscription < ApplicationRecord
     end
   end
 
-  def unique_email
-    self.errors.add(:user_email, I18n.t('subscription.errors.email_exist') ) if User.where(email: self.user_email).exists?
-  end
-
+  private
+    def unique_email
+      errors.add(:user_email, I18n.t('subscription.errors.email_exist') ) if User.where(email: user_email).exists?
+    end
 end
