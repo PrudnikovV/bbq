574a9af1717779fab62c0f9e53ee8cf91f2c3a5d
prudnikovv <prudnikovv@ukr.net>
2020-01-17 11:51:28 +0000

    add config aws

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,9 +5,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
-    storage :gcloud
+    storage :fog
   else
-    storage :gcloud
+    storage :file
   end
   # storage :fog
 
